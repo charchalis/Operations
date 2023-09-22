72698cc0e9a5c3b210ce198f6a6000489cd9160e
charchalis <charchalis97@gmail.com>
2023-09-22 12:08:18 +0100

    multiple ops

--- Operations/src/main/java/org/example/Application.java
+++ Operations/src/main/java/org/example/Application.java
@@ -4,8 +4,19 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String operation = scanner.nextLine();
+
+
+
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-        System.out.println("Result: " + (a + b));
+
+        int result;
+
+        if (operation.equals("sum")) result = a+b;
+        else result = a*b;
+
+        System.out.println("Result: " + result);
     }
 }
